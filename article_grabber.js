310de9b89c23ca561ed1b121a00c606b94e55fda
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-08-28 18:25:33 +0300

    removed console logs

--- article_grabber.js
+++ article_grabber.js
@@ -35,7 +35,6 @@ app.get ( '/' , function ( web_req , web_res ) {
 	{
 		timestamp = web_req.query.date ;
 		date = new Date ( parseInt(timestamp) ) ;
-		console.log ( timestamp + "	" + date ) ;
 		date = date.toISOString();
 		query = "SELECT DISTINCT url, id , title, text, image , 1000 * UNIX_TIMESTAMP(created_at) AS date FROM articles WHERE `created_at` > '"+date+"' AND `feed` = '"+feed+"' ORDER BY created_at DESC" ;
 	}
